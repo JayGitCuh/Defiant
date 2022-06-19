82cfe46599ca322fe8f9c7c2c69ebc95287462e7
Jay <94145603+JayGitCuh@users.noreply.github.com>
2022-06-19 00:49:35 -0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 286090429 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/JayGitCuh/Lua-Stuff/main/Jay%20Framework/loader.lua', true))()
+else
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/JayGitCuh/Lua-Stuff/main/Jay%20Framework/loader.lua', true))()
+end
